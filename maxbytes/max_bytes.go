e1d8b9f35d5454ffc8b446b24ad04bcb4292276f
Sergey Solodyagin <solodyagin@gmail.com>
2023-07-07 16:52:21 +0300

    hotfix

--- maxbytes/max_bytes.go
+++ maxbytes/max_bytes.go
@@ -9,7 +9,7 @@ import (
 // MaxBytes returns a middleware that limit reading of http request body.
 func MaxBytes(n int64) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if n >= 0 && c.Request.ContentLength > n {
+		if n > 0 && c.Request.ContentLength > n {
 			c.AbortWithStatus(http.StatusRequestEntityTooLarge)
 		}
 	}
