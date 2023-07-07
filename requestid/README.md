76ca310ccec7b308aa1afb789ffdae6db3bfd23f
Sergey Solodyagin <solodyagin@gmail.com>
2023-07-07 17:07:09 +0300

    feat: add requestid

---
+++ requestid/README.md
@@ -0,0 +1,72 @@
+# RequestID
+
+Request ID middleware for Gin Framework. Adds an indentifier to the response using the `X-Request-ID` header. Passes the `X-Request-ID` value back to the caller if it's sent in the request headers.
+
+Copied from [gin-contrib/requestid](https://github.com/gin-contrib/requestid)
+
+## Config
+
+define your custom generator function:
+
+```go
+func main() {
+
+  r := gin.New()
+
+  r.Use(
+    requestid.New(
+      requestid.WithGenerator(func() string {
+        return "test"
+      }),
+      requestid.WithCustomHeaderStrKey("your-customer-key"),
+    ),
+  )
+
+  // Example ping request.
+  r.GET("/ping", func(c *gin.Context) {
+    c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
+  })
+
+  // Listen and Server in 0.0.0.0:8080
+  r.Run(":8080")
+}
+```
+
+## Example
+
+```go
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "time"
+
+  "github.com/andoma-go/requestid"
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+
+  r := gin.New()
+
+  r.Use(requestid.New())
+
+  // Example ping request.
+  r.GET("/ping", func(c *gin.Context) {
+    c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
+  })
+
+  // Listen and Server in 0.0.0.0:8080
+  r.Run(":8080")
+}
+```
+
+How to get the request identifier:
+
+```go
+// Example / request.
+r.GET("/", func(c *gin.Context) {
+  c.String(http.StatusOK, "id:"+requestid.Get(c))
+})
+```
---
+++ requestid/requestid_test.go
@@ -0,0 +1,131 @@
+package requestid
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+const testXRequestID = "test-request-id"
+
+func emptySuccessResponse(c *gin.Context) {
+	c.String(http.StatusOK, "")
+}
+
+func Test_RequestID_CreateNew(t *testing.T) {
+	r := gin.New()
+	r.Use(New())
+	r.GET("/", emptySuccessResponse)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, w.Header().Get(headerXRequestID))
+}
+
+func Test_RequestID_PassThru(t *testing.T) {
+	r := gin.New()
+	r.Use(New())
+	r.GET("/", emptySuccessResponse)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	req.Header.Set(headerXRequestID, testXRequestID)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, testXRequestID, w.Header().Get(headerXRequestID))
+}
+
+func TestRequestIDWithCustomID(t *testing.T) {
+	r := gin.New()
+	r.Use(
+		New(
+			WithGenerator(func() string {
+				return testXRequestID
+			}),
+		),
+	)
+	r.GET("/", emptySuccessResponse)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, testXRequestID, w.Header().Get(headerXRequestID))
+}
+
+func TestRequestIDWithCustomHeaderKey(t *testing.T) {
+	r := gin.New()
+	r.Use(
+		New(
+			WithCustomHeaderStrKey("customKey"),
+		),
+	)
+	r.GET("/", emptySuccessResponse)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	req.Header.Set("customKey", testXRequestID)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, testXRequestID, w.Header().Get("customKey"))
+}
+
+func TestRequestIDWithHandler(t *testing.T) {
+	r := gin.New()
+	called := false
+	r.Use(
+		New(
+			WithHandler(func(c *gin.Context, requestID string) {
+				called = true
+				assert.Equal(t, testXRequestID, requestID)
+			}),
+		),
+	)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	req.Header.Set("X-Request-ID", testXRequestID)
+	r.ServeHTTP(w, req)
+
+	assert.True(t, called)
+}
+
+func TestRequestIDIsAttachedToRequestHeaders(t *testing.T) {
+	r := gin.New()
+
+	r.Use(New())
+
+	r.GET("/", func(c *gin.Context) {
+		result := c.GetHeader(defaultHeaderKey)
+		assert.NotEmpty(t, result)
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	r.ServeHTTP(w, req)
+}
+
+func TestRequestIDNotNilAfterGinCopy(t *testing.T) {
+	r := gin.New()
+	r.Use(New())
+
+	r.GET("/", func(c *gin.Context) {
+		copy := c.Copy()
+		result := Get(copy)
+		assert.NotEmpty(t, result)
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+	r.ServeHTTP(w, req)
+}
